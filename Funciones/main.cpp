367f08673067a3d9cfafd446614127009d219dbb
DaniVal875 <24020060@uadeo.mx>
2024-09-12 10:07:15 -0700

    Add files via upload

---
+++ Funciones/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Hola Mundo");
+    printf("\n Hola Mundo");
+    printf("\nX    \nY");
+    printf("\n\n Hola Mundo \a");
+    printf("\n\n  \"Hola Mundo\"");
+}
