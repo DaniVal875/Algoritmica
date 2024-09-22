644af0a5e0892a1775779a4bafe16937efabd328
DaniVal875 <24020060@uadeo.mx>
2024-09-22 07:53:35 -0700

    Add files via upload

---
+++ Nombres/main_iostream.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char nombre[20], apellido[20];
+
+    cout <<"Introduzca su nombre: ";
+    cin >>nombre;
+
+    cout <<"Introduzca su apellido: ";
+    cin >>apellido;
+
+    cout <<"Hola " << nombre <<" " << apellido;
+}
